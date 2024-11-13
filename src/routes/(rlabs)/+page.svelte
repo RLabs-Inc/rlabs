29f0f7802116325df908f27e277e5960b14c2bc3
Rodrigo Luglio <rodrigo@rlabs.com.br>
2024-11-13 00:58:51 -0300

    home redirected to vscode-themes-community

--- src/routes/(rlabs)/+page.svelte
+++ src/routes/(rlabs)/+page.svelte
@@ -1,3 +1,12 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { goto } from '$app/navigation';
+
+  onMount(() => {
+    goto('/vscode-themes-community');
+  });
+</script>
+
 <div class="flex h-full flex-col items-center justify-center">
   <h1 class="text-4xl font-bold">Welcome to RLabs Inc.</h1>
 </div>
