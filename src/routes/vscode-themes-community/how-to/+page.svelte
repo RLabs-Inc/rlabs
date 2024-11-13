96dd6edacc7b03c0802f2cd37fff73f2a4ea5034
Rodrigo Luglio <rodrigo@rlabs.com.br>
2024-11-13 07:39:06 -0300

    fix how to comming soon

--- src/routes/vscode-themes-community/how-to/+page.svelte
+++ src/routes/vscode-themes-community/how-to/+page.svelte
@@ -8,7 +8,7 @@
   />
 </svelte:head>
 
-<div class="flex min-h-[calc(100vh-2rem)] items-center justify-center">
+<div class="flex min-h-[calc(100vh-2rem)] flex-col items-center justify-center">
   <h1 class="text-4xl font-black">How to use the VSCode theme generator</h1>
   <p class="text-lg">Instructions coming soon...</p>
 </div>
