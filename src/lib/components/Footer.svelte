27ff048275fc799eae2099374fbea91f96e46d9e
Rodrigo Luglio <rodrigo@rlabs.com.br>
2024-11-13 14:12:35 -0300

    github link to repo in footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,6 @@
-<script lang="ts"></script>
+<script lang="ts">
+  import { Button } from '$lib/components/ui/button';
+</script>
 
 <footer
   class="sticky bottom-0 z-50 flex flex-col items-center justify-center bg-background/40 backdrop-blur-lg"
@@ -8,10 +10,18 @@
     <img src="/images/RLabs-Lamp.png" alt="RLabs" class="w-4" />
     <div class="w-full border-t border-border"></div>
   </div>
-  <span class="text-xs">
-    &copy;
-    {new Date().getFullYear()} - RLabs Inc.
-  </span>
+  <div class="-mt-[0.5rem] flex w-full items-center justify-end gap-1 px-7 py-0">
+    <!-- <div class="h-4 w-4"></div> -->
+
+    <Button href="https://github.com/RLabs-Inc/rlabs" target="_blank" variant="link" size="icon">
+      <img src="/images/github-mark.svg" alt="GitHub" class="h-5 w-5 dark:invert" />
+      <span class="sr-only">GitHub</span>
+    </Button>
+    <span class="text-sm">
+      &copy;
+      {new Date().getFullYear()} - RLabs Inc.
+    </span>
+  </div>
 </footer>
 
 <style></style>
