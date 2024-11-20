3b6fe5056fd24e49b0f57da98f04c5f40a45f051
Rodrigo Luglio <rodrigo@rlabs.com.br>
2024-11-20 16:07:30 -0300

    file name on mock tab

--- src/lib/components/vscode/preview/vsc-mock/EditorTabs.svelte
+++ src/lib/components/vscode/preview/vsc-mock/EditorTabs.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import EditorTab from './EditorTab.svelte';
+  import { getSelectedFile } from '$lib/state/vscode/editor.svelte';
+  const selectedFile = getSelectedFile();
 
   const tabs = [
-    { name: 'main.svelte', active: true },
     { name: 'Header.svelte', active: false },
     { name: 'Footer.svelte', active: false }
   ];
@@ -10,6 +11,7 @@
 
 <div class="flex flex-col">
   <div class="flex" style="background-color: var(--bg1); transition: all 200ms ease-in-out;">
+    <EditorTab name={selectedFile().file.name} active={true} />
     {#each tabs as tab}
       <EditorTab name={tab.name} active={tab.active} />
     {/each}
