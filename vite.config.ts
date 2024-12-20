8dbd72cceff48cd34cd8fbef683abe4301ff28fa
Rodrigo Luglio <rodrigo@rlabs.com.br>
2024-12-19 21:33:06 -0300

    monaco

--- vite.config.ts
+++ vite.config.ts
@@ -1,60 +1,11 @@
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
-import { monaco } from './monaco.plugin';
 
 export default defineConfig({
-  plugins: [
-    sveltekit(),
-    enhancedImages(),
-    monaco({
-      languages: [
-        'typescript',
-        'javascript',
-        'html',
-        'css',
-        'scss',
-        'vue',
-        'vue-html',
-        'svelte',
-        'tsx',
-        'ruby',
-        'php',
-        'go',
-        'java',
-        'kotlin',
-        'dart',
-        'csharp',
-        'python',
-        'rust',
-        'swift',
-        // 'cpp',
-        'c',
-        'lua',
-        'sql',
-        'yaml',
-        'json',
-        'bash',
-        'markdown'
-      ],
-      features: [
-        'theme',
-        'minimap'
-      ],
-      globalAPI: false
-    })
-  ],
+  plugins: [sveltekit(), enhancedImages()],
   ssr: {
     // Exclude monaco-editor from SSR
     external: ['monaco-editor-core']
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'monaco-editor': ['monaco-editor-core/esm/vs/editor/editor.api']
-        }
-      }
-    }
   }
 });
